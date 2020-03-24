51fb70a3d0c1d3dae0cbaec1f5bd15421ca01c56
doku <dragon.ice@foxmail.com>
2020-03-24 16:37:12 +0800

    add

---
+++ FD.Util.Web/Model/PageRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FD.Util.Web
+{
+    public class PageRequest
+    {
+        public int page_num { get; set; }
+
+        public int page_size { get; set; }
+
+        public string order_by { get; set; }
+    }
+}
