ef0bd30d3abf32f63dd168e599fb85f28ca3e87a
doku <dragon.ice@foxmail.com>
2020-08-25 17:32:51 +0800

    -a

---
+++ FD.Util.Test/Util/StringHelperTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FD.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FD.Util.Tests
+{
+    [TestClass()]
+    public class StringHelperTests
+    {
+        [TestMethod()]
+        public void GetNumTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void validateNumTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsValidDateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsValidEmailTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void validatePhoneTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void validateFaxTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsDateStringTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FilePathHasInvalidCharsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ReplaceFilePathInvalidCharsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FilterCharTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
