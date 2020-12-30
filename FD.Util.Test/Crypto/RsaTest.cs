c4c60827fe585c7ee63101fb7f9f937fde170683
doku <dragon.ice@foxmail.com>
2020-12-30 19:11:31 +0800

    -i rsahelper

--- FD.Util.Test/Crypto/RsaTest.cs
+++ FD.Util.Test/Crypto/RsaTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 using FD.Util.Crypto;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -7,6 +8,24 @@ namespace FD.Util.Test
     [TestClass]
     public class RsaTest
     {
+        [TestMethod]
+        public void EncryptTest()
+        {
+            var rsa = new RSACryptoServiceProvider();
+
+
+            var pubK = rsa.ToXmlString(false);
+
+            var priK = rsa.ToXmlString(true);
+
+            var cipher = RSAHelper.Encrypt("cszfp.com", pubK);
+
+            var clear = RSAHelper.Decrypt(cipher, priK);
+
+            Assert.AreEqual("cszfp.com", clear);
+
+        }
+
         [TestMethod]
         public void ExportPublicKeyTest()
         {
